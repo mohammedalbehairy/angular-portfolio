ac89437ed3d933226be6fa3857a26cc6ad9a49cb
mohammedalbehairy <mohammedalbehairy90@gmail.com>
2019-12-25 14:52:18 +0200

    created basic components

--- angularPortofolio/src/app/app-routing.module.ts
+++ angularPortofolio/src/app/app-routing.module.ts
@@ -1,8 +1,58 @@
+import { BlogDetailsComponent } from './components/blogs/blog-details/blog-details.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: 'blogs',
+    component: BlogsComponent
+  },
+  {
+    path: 'blogs/:id',
+    component: BlogDetailsComponent
+  },
+  {
+    path: 'pricing',
+    component: PricingComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularPortofolio/src/app/app.module.ts
+++ angularPortofolio/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { BlogDetailsComponent } from './components/blogs/blog-details/blog-details.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { PricingComponent } from './components/pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    BlogsComponent,
+    BlogDetailsComponent,
+    PageNotFoundComponent,
+    ContactUsComponent,
+    PricingComponent,
   ],
   imports: [
     BrowserModule,
